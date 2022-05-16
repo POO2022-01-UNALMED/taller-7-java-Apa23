a997e5aab8d16b776bb222c95ab6abedce35a29b
Andres Felipe Aparicio Mestre <anapariciom@unal.edu.co>
2022-05-16 13:14:22 -0500

    Commit 1

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,35 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	private String[] letras;
+	private String interpretacion;	
+
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+	public String[] getLetras() {
+		return letras;
+	}
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	public int cantidadLetras() {}
+	
+	public String interpretacion() {}
+	
+	public String toString() {}
+
+}
---
+++ src/main/java/comunicacion/Libro.java
@@ -0,0 +1,47 @@
+package comunicacion;
+
+public class Libro extends Escrito {
+	private String co_autor;
+	private String editor;
+	private String edicion;
+	private String interpretacion;
+	public Libro(String origen, String titulo, String autor, int paginas, String co_autor, String editor,
+			String edicion, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.co_autor = co_autor;
+		this.editor = editor;
+		this.edicion = edicion;
+		this.interpretacion = interpretacion;
+	}
+	public String getCo_autor() {
+		return co_autor;
+	}
+	public void setCo_autor(String co_autor) {
+		this.co_autor = co_autor;
+	}
+	public String getEditor() {
+		return editor;
+	}
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+	public String getEdicion() {
+		return edicion;
+	}
+	public void setEdicion(String edicion) {
+		this.edicion = edicion;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	public int palabrasTotales(int x) {}
+	
+	public String interpretacion() {}
+	
+	public String toString() {}	
+	
+}
